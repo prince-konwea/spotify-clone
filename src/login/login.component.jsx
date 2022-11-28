e013f26761f21fbb5133c4b13f03e78c0cdbb1ad
prince-konwea <kokodecontee123@gmail.com>
2022-11-28 11:54:23 +0100

    created login component for project

---
+++ src/login/login.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./login.styles.css"
+
+function Login() {
+  return (
+    <div className='login'>
+      
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt='spotify-logo' />
+      <a>Login with spotify</a>
+    </div>
+  )
+}
+
+export default Login;
